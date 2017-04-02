29d80093cd70c1ed59a877effc3330d4d38690c6
Max Yi-Hsun Chou <mrttra@Maxs-Air.local>
2017-04-03 07:23:02 +0800

    add longer

--- app.js
+++ app.js
@@ -33,11 +33,20 @@ app.use(session(sessionConfig, app))
 // middlewares
 app.use(convert(bodyparser))
 app.use(convert(json()))
+app.use(convert(logger()))
 app.use(require('koa-static')(__dirname + '/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
+
+// logger
+app.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+})
 
 router.use('/ask', ask.routes(), ask.allowedMethods())
 router.use('/users', users.routes(), users.allowedMethods())
